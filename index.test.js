6c85d80d14de85e22f5e1809aafaed94ad01e0db
hcl2020 <907953320@qq.com>
2022-06-22 11:47:56 +0800

    Simple test

--- index.test.js
+++ index.test.js
@@ -1,17 +1,25 @@
-const postcss = require('postcss')
+const postcss = require('postcss');
 
-const plugin = require('./')
+const plugin = require('./');
 
-async function run (input, output, opts = { }) {
-  let result = await postcss([plugin(opts)]).process(input, { from: undefined })
-  expect(result.css).toEqual(output)
-  expect(result.warnings()).toHaveLength(0)
+async function testEqualResult(input, output, opts = {}) {
+  let result = await postcss([plugin(opts)]).process(input, { from: undefined });
+  expect(result.css).toEqual(output);
+  expect(result.warnings()).toHaveLength(0);
 }
 
-/* Write tests here
+it('with overflow: auto', async () => {
+  await testEqualResult('div{ overflow: auto; }', 'div{ overflow: auto; overflow: overlay; }', {});
+});
 
-it('does something', async () => {
-  await run('a{ }', 'a{ }', { })
-})
+it('with overflow-x: auto', async () => {
+  await testEqualResult('div{ overflow-x: auto; }', 'div{ overflow-x: auto; overflow-x: overlay; }', {});
+});
 
-*/
+it('with overflow-y: auto', async () => {
+  await testEqualResult('div{ overflow-y: auto; }', 'div{ overflow-y: auto; overflow-y: overlay; }', {});
+});
+
+it('with overflow: scroll', async () => {
+  await testEqualResult('div{ overflow: scroll; }', 'div{ overflow: scroll; }', {});
+});

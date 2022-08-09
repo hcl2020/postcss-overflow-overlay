4b4bb8fa51cf332dfc055cb43b10be32453266b0
hcl2020 <907953320@qq.com>
2022-08-09 09:38:05 +0800

    feat: 覆盖更多测试用例

--- index.js
+++ index.js
@@ -5,15 +5,23 @@ module.exports = () => {
   let skipped = Symbol('isSkipped'); // skipped flag
   let counter = Symbol('skippedCounter'); // counter for test "isSkipped" optimization
 
+  /**
+   * @param {import('postcss').Declaration} decl
+   */
   function makeRuleOverflowOverlay(decl) {
     let rule = decl.parent;
     rule[counter] = Number.isInteger(rule[counter]) ? rule[counter] : 0;
     if (!rule[skipped]) {
-      if (decl.value === 'auto') {
-        let hasOverlay = rule.some(i => i.value === 'overlay');
-        if (!hasOverlay) {
-          rule.append({ prop: decl.prop, value: 'overlay' });
+      let values = decl.value.split(' ');
+      if (values.includes('auto')) {
+        const annotation = decl.prev();
+        if (annotation && annotation.type === 'comment') {
+          if (/overflow-overlay:\s*(off)/i.test(annotation.text)) {
+            annotation.remove();
+            return;
+          }
         }
+        decl.cloneAfter({ prop: decl.prop, value: values.map(v => (v === 'auto' ? 'overlay' : v)).join(' '), important: decl.important });
         rule[skipped] = true;
         rule[counter]++;
       }
--- index.test.js
+++ index.test.js
@@ -2,6 +2,8 @@ const postcss = require('postcss');
 
 const plugin = require('./');
 
+const css = String.raw;
+
 async function testEqualResult(input, output, opts = {}) {
   let result = await postcss([plugin(opts)]).process(input, { from: undefined });
   expect(result.css).toEqual(output);
@@ -9,17 +11,211 @@ async function testEqualResult(input, output, opts = {}) {
 }
 
 it('with overflow: auto', async () => {
-  await testEqualResult('div{ overflow: auto; }', 'div{ overflow: auto; overflow: overlay; }', {});
+  await testEqualResult(
+    css`
+      div {
+        overflow: auto;
+      }
+    `,
+    css`
+      div {
+        overflow: auto;
+        overflow: overlay;
+      }
+    `,
+    {}
+  );
+});
+
+it('with double overflow: auto', async () => {
+  await testEqualResult(
+    css`
+      div {
+        overflow: auto;
+        overflow: auto;
+      }
+    `,
+    css`
+      div {
+        overflow: auto;
+        overflow: overlay;
+        overflow: auto;
+      }
+    `,
+    {}
+  );
+});
+
+it('with overflow: auto !important', async () => {
+  await testEqualResult(
+    css`
+      div {
+        overflow: auto !important;
+      }
+    `,
+    css`
+      div {
+        overflow: auto !important;
+        overflow: overlay !important;
+      }
+    `,
+    {}
+  );
+});
+
+it('skip overflow: auto', async () => {
+  await testEqualResult(
+    css`
+      div {
+        /** overflow-overlay: off */
+        overflow: auto;
+      }
+    `,
+    css`
+      div {
+        overflow: auto;
+      }
+    `,
+    {}
+  );
 });
 
 it('with overflow-x: auto', async () => {
-  await testEqualResult('div{ overflow-x: auto; }', 'div{ overflow-x: auto; overflow-x: overlay; }', {});
+  await testEqualResult(
+    css`
+      div {
+        overflow-x: auto;
+      }
+    `,
+    css`
+      div {
+        overflow-x: auto;
+        overflow-x: overlay;
+      }
+    `,
+    {}
+  );
 });
 
 it('with overflow-y: auto', async () => {
-  await testEqualResult('div{ overflow-y: auto; }', 'div{ overflow-y: auto; overflow-y: overlay; }', {});
+  await testEqualResult(
+    css`
+      div {
+        overflow-y: auto;
+      }
+    `,
+    css`
+      div {
+        overflow-y: auto;
+        overflow-y: overlay;
+      }
+    `,
+    {}
+  );
 });
 
 it('with overflow: scroll', async () => {
-  await testEqualResult('div{ overflow: scroll; }', 'div{ overflow: scroll; }', {});
+  await testEqualResult(
+    css`
+      div {
+        overflow: scroll;
+      }
+    `,
+    css`
+      div {
+        overflow: scroll;
+      }
+    `,
+    {}
+  );
+});
+
+it('with overflow: scroll and auto', async () => {
+  await testEqualResult(
+    css`
+      div {
+        overflow: scroll;
+        overflow: auto;
+      }
+    `,
+    css`
+      div {
+        overflow: scroll;
+        overflow: auto;
+        overflow: overlay;
+      }
+    `,
+    {}
+  );
+});
+
+it('with overflow: auto and scroll', async () => {
+  await testEqualResult(
+    css`
+      div {
+        overflow: auto;
+        overflow: scroll;
+      }
+    `,
+    css`
+      div {
+        overflow: auto;
+        overflow: overlay;
+        overflow: scroll;
+      }
+    `,
+    {}
+  );
+});
+
+it('with overflow: hidden scroll', async () => {
+  await testEqualResult(
+    css`
+      div {
+        overflow: hidden scroll;
+      }
+    `,
+    css`
+      div {
+        overflow: hidden scroll;
+      }
+    `,
+    {}
+  );
+});
+
+it('with overflow: hidden auto', async () => {
+  await testEqualResult(
+    css`
+      div {
+        overflow: hidden auto;
+      }
+    `,
+    css`
+      div {
+        overflow: hidden auto;
+        overflow: hidden overlay;
+      }
+    `,
+    {}
+  );
+});
+
+it('with overflow: auto hidden', async () => {
+  await testEqualResult(
+    css`
+      div {
+        height: auto;
+        overflow: auto hidden;
+      }
+    `,
+    css`
+      div {
+        height: auto;
+        overflow: auto hidden;
+        overflow: overlay hidden;
+      }
+    `,
+    {}
+  );
 });
